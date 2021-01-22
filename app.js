c76494feda1105cd2853970363edffe338c76aa5
Ralph Oliveira <ralphive@protonmail.com>
2021-01-22 10:37:21 +0000

    ui routing

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const path = require('path');
 const app = express()
- 
+const port = process.env.PORT || 8080 
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+app.get('/kitchen', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/kitchen.html'));
+})
+
+app.get('/delivery', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/delivery.html'));
 })
  
-app.listen(3000)
+app.listen(port, function () {
+    console.log('ByD Eats listening on port ' + port);
+});
