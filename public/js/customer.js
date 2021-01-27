96f9ea818f27d3b1c0706b74c4947c133e261be1
Ralph Oliveira <ralphive@protonmail.com>
2021-01-27 13:18:35 +0000

    form to create orders on workflow (to be replaced by chatbot)

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,13 +1,20 @@
 function startFlow() {
-  let data = {};
-  let formdata = new FormData(theform);
-  for (let tuple of formdata.entries()) data[tuple[0]] = tuple[1];
-  
-  // construct an HTTP request
-  var xhr = new XMLHttpRequest();
-  xhr.open(form.method, form.action, true);
-  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  
-  // send the collected data as JSON
-  xhr.send(JSON.stringify(data));
+
+  var data = {
+    recipient: $("#recipient").val(),
+    orderData: {
+        CustomerName: $("#CustomerName").val(),
+        Address: $("#Address").val(),
+        ProductID: $("#ProductID").val()
+    }
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "api/start",
+    contentType: "application/json", 
+    data: JSON.stringify(data),
+    success: function(res, status){
+      alert("Workflow ID: " + res.id + " Created!")}
+    });
 }
